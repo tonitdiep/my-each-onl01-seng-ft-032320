34c17b07eb4fa710dc61754bba1477fef09a0e31
Toni  Diep <adiep123@gmail.com>
2020-04-01 00:48:05 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)  # put argument(s) her
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+    array
 end
+
+#each method accessing to the array
+#look thru each one
+#yield to each element it makes contact
+#to grab that information
+#return value
